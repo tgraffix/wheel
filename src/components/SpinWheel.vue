ac9202161b5e6c152c4a5d039ddb2e4100a0a8e9
tgraffix <8551903+tgraffix@users.noreply.github.com>
2025-01-30 23:53:31 -0600

    Update SpinWheel.vue

--- src/components/SpinWheel.vue
+++ src/components/SpinWheel.vue
@@ -83,7 +83,7 @@ const properties: WheelProps = {
 
 const container = ref();
 
-let spinCount = 20;
+let spinCount = 10;
 let wheel: Wheel | undefined = undefined;
 
 const stopAndClearSound = () => {
@@ -116,10 +116,10 @@ const spin = () => {
     // Provide a more entertaining performance.
     switch (true) {
       case wheel.rotationSpeed < 400:
-        wheel.rotationResistance = -100;
+        wheel.rotationResistance = -400;
         break;
       case wheel.rotationSpeed < 100:
-        wheel.rotationResistance = -30;
+        wheel.rotationResistance = -50;
         break;
       case wheel.rotationSpeed < 30:
         wheel.rotationResistance = -10;
