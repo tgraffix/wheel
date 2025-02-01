811017b8fea09117c25f61a415e9c26334f8c940
tgraffix <8551903+tgraffix@users.noreply.github.com>
2025-01-31 19:55:42 -0600

    Update SpinWheel.vue

--- src/components/SpinWheel.vue
+++ src/components/SpinWheel.vue
@@ -116,13 +116,13 @@ const spin = () => {
     // Provide a more entertaining performance.
     switch (true) {
       case wheel.rotationSpeed < 400:
-        wheel.rotationResistance = -400;
+        wheel.rotationResistance = -600;
         break;
       case wheel.rotationSpeed < 100:
         wheel.rotationResistance = -50;
         break;
       case wheel.rotationSpeed < 30:
-        wheel.rotationResistance = -10;
+        wheel.rotationResistance = -100;
         break;
     }
   };
